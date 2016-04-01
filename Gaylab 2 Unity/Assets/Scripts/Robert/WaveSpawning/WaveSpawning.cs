78db4ce5182577624ae63efa474d20c2ae59f171
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2016-04-01 09:55:19 +0200

    WaveSpawning

---
+++ Gaylab 2 Unity/Assets/Scripts/Robert/WaveSpawning/WaveSpawning.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawning : MonoBehaviour {
+
+	public int waveNumber;
+	public int startNumber;
+	public int enemyNumber;
+
+	public GameObject enemyPrefab;
+
+	public bool enemysDead;
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public int NumberOfEnemeys (){
+		
+
+		return enemyNumber;
+	}
+
+	public void StartWaveSpawning (){
+		enemysDead = true;
+		NumberOfEnemeys();
+		for(int i = 0; i < enemyNumber; i++){
+
+		}
+		
+	}
+
+	public void CheckIfWaveDone (){
+
+	}
+}
