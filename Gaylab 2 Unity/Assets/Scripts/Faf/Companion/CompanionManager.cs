73d5a78257fbcf04fb293dfa8fd8fe4d07f14522
Fafanur <senolfatmanur@gmail.com>
2016-04-26 12:28:35 +0200

    Companion

---
+++ Gaylab 2 Unity/Assets/Scripts/Faf/Companion/CompanionManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompanionManager : MonoBehaviour
+{
+    public enum CompMode { Regularstate, Followstate, Scoutstate, IdleState };
+    public CompMode compMode;
+
+    public Transform target;
+    NavMeshAgent agent;
+
+    public bool hasMoved;
+    public GameObject companionCanvas;
+    public Transform scoutTarget;
+    public int flyHeight;
+    public float followDis;
+
+    public float fOWRadius;
+    public Transform sphereCastTar; // Should be on the ground, onder de companion.
+    public LayerMask enemyLayermask;
+    public Collider[] EnemyColliders;
+
+    void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update ()
+    {
+        Move();
+        switch (compMode)
+        {
+            case CompMode.Regularstate:
+                Move();
+                break;
+            case CompMode.Followstate:
+                FollowMoving();
+                break;
+            case CompMode.Scoutstate:
+                Scouting();
+                // komt nog
+                break;
+            case CompMode.IdleState:
+                // does nothing
+                break;
+        }
+    }
+
+    public void Move()
+    {
+        agent.SetDestination(target.position);
+        transform.position = new Vector3(transform.position.x, flyHeight, transform.position.z);
+    }
+
+    public void FollowMoving()
+    {
+        float distance;
+        distance = Vector3.Distance(transform.position, scoutTarget.position);
+
+        agent.SetDestination(scoutTarget.position);
+        transform.position = new Vector3(transform.position.x, flyHeight, transform.position.z);
+        // print(distance);
+        if (distance < followDis) // fix so that the scoutmode doesnt loop 
+        {
+            compMode = CompMode.Scoutstate;
+            hasMoved = true;
+        } 
+    }
+
+    public void Scouting()
+    {
+        EnemyColliders = Physics.OverlapSphere(sphereCastTar.position, fOWRadius, enemyLayermask);
+        // shows how many enemy colliders are seen in an array
+        foreach(Collider hitcol in EnemyColliders)
+        {
+            Debug.Log(hitcol.gameObject);
+        }
+
+        agent.SetDestination(scoutTarget.position);
+        transform.position = new Vector3(transform.position.x, flyHeight, transform.position.z);
+
+        if (hasMoved == true && Input.GetButtonDown("PickUp"))
+        {
+            companionCanvas.SetActive(true);
+        }
+    }
+
+    public void RelocateBut()
+    {
+        hasMoved = false;
+        compMode = CompMode.Followstate;
+        companionCanvas.SetActive(false);
+    }
+    
+    public void CallBackBut()
+    {
+        hasMoved = false;
+        companionCanvas.SetActive(false);
+        compMode = CompMode.Regularstate;
+    }
+}
---
+++ Gaylab 2 Unity/Assets/Scripts/Faf/Companion/ScoutingIndicator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoutingIndicator : MonoBehaviour
+{
+   // public GameObject particle;
+   // public bool compMayMove;
+    public int rayDis;
+
+    public RaycastHit hit;
+    public Transform rayEndPos;
+
+    public CompanionManager compScript;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("PickUp") && compScript.hasMoved == false ) // Or any other button asigned to it 
+        {
+                ShootCompanion();
+        }
+    }
+    void ShootCompanion()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position, fwd, Color.blue);
+
+        if (Physics.Raycast(transform.position, fwd, out hit, rayDis))
+        {
+
+            print("i hit something");
+            print(hit.point);
+            rayEndPos.position = hit.point;
+            compScript.compMode = CompanionManager.CompMode.Followstate;
+        }
+
+    }
+}
