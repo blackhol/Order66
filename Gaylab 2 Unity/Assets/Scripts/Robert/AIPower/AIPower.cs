017df624328dbee708351ab6f4b53750c3722dbd
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2016-05-30 09:37:17 +0200

    Some Updates and birds

---
+++ Gaylab 2 Unity/Assets/Scripts/Robert/AIPower/AIPower.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIPower : MonoBehaviour {
+	public int baseDamage;
+	public int maxDamage;
+	public int calculatetDamage;
+
+	void DoDamage (Transform enemy){
+		//grab the oponent CalculateDamage() - hp;
+	}
+	int CalculateDamage () {
+		calculatetDamage = Random.Range(baseDamage,maxDamage);
+		return calculatetDamage;
+	}
+}
