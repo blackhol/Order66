e461c1d4c9735a36ffad806d9f06c06bfe972f8a
Fafanur <senolfatmanur@gmail.com>
2016-06-07 16:15:59 +0200

    CompCannister

---
+++ Gaylab 2 Unity/Assets/Scripts/Faf/Companion/Companion Cannister/CompPickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompPickup : MonoBehaviour
+{
+    public CompanionManager compManager;
+    // Particle system moet op eigen object.
+    void Start()
+    {
+        gameObject.GetComponent<ParticleSystem>().Play();
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            compManager.pickups++;
+            Destroy(gameObject);
+        }
+    }
+}
